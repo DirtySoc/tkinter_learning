f104511e055b3d4ebc62cfd6b18f794e9a8e3ad8
Derek <derekwillingham.401@gmail.com>
2018-08-28 19:27:36 -0400

    created main.py and updated calc.py, gui_parts.py

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
-import tkinter as tk
-import src.gui_parts as mygui
+# pylint: disable=W0614
+from tkinter import *
+from tkinter.ttk import *
 
+# from src.gui_parts import MyFirstGUI, MyFirstGUI_binds, MyFirstGUI_grid
+# from src.calc import Calculator
+
+class MainView:
+    """A GUI for selecting what tutorial GUI you would like to show.
+    """
+    def __init__(self, master):
+        """ 
+            Args:
+                master: a instance of the toplevel widget from tkinter: 
+                        tkinter.Tk()        
+        """ 
+        self.master = master
+        master.title = ("tkinter Tutorials")
+        
+        # Create widgets...
+        self.label = Label(master, text="Welcome to this tutorial. Select a GUI to launch:")
+        self.button1 = Button(master, text="MyFirstGUI", command=self.testbutton)
+        # self.button1 = Button(master, text="MyFirstGUI", command=lambda: self.testbutton("Button 1 Pressed!"))
+        # https://stackoverflow.com/questions/20933639/tkinter-button-events-firing-on-load#20933659
+        
+
+        # Add widgets to view...
+        self.label.grid()
+        self.button1.grid()
+
+    # def testbutton(self, buttonName):
+    #     print(buttonName)
+    # https://stackoverflow.com/questions/20933639/tkinter-button-events-firing-on-load#20933659
+    def testbutton(self):
+        print("Button Pressed!")
 
 if __name__ == '__main__':
-    root = tk.Tk()
-    my_gui = mygui.MyFirstGUI(root)
+    root = Tk()
+    my_gui = MainView(root)
     root.mainloop()
-
-"""
-The below does not work... :(
-"""
-# from multiprocessing import Process
-# if __name__ == '__main__':
-    
-#     processes = []
-    
-#     # root = tk.Tk()
-#     processes.append(Process(target=mygui.MyFirstGUI(tk.Tk())))
-#     processes.append(Process(target=mygui.MyFirstGUI_grid(tk.Tk())))
-    
-#     for process in processes:
-#         process.start()
-    
-#     for process in processes:
-#         process.join()
-
-#     pass
--- src/gui_parts.py
+++ src/gui_parts.py
@@ -8,12 +8,12 @@ class MyFirstGUI:
     def __init__(self, master):
         """ 
             Args:
-                master: a instance of the toplevel widget from tkinter. 
-                tkinter.Tk()        
+                master: a instance of the toplevel widget from tkinter: 
+                        tkinter.Tk()        
         """ 
         self.master = master
         master.title("A simple GUI")
-        
+
         # Create the widgets...
         self.label = Label(master, text="This is our first GUI!")
         self.greet_button = Button(master, text="Greet", command=self.greet)
@@ -36,8 +36,8 @@ class MyFirstGUI_grid:
     def __init__(self, master):
         """ 
             Args:
-                master: a instance of the toplevel widget from tkinter. 
-                tkinter.Tk()        
+                master: a instance of the toplevel widget from tkinter: 
+                        tkinter.Tk()        
         """ 
         self.master = master
         master.title("A simple GUI")
@@ -54,6 +54,42 @@ class MyFirstGUI_grid:
 
     def greet(self):
         print("Greetings!")
+
+class MyFirstGUI_binds:
+    """Similar to MyFirstGUI but includes the bind method to add an event
+    handler to the label that is called when the label is clicked with the LMB
+    """
+    LABEL_TEXT = [
+        "This is our first GUI!",
+        "Actually, this is our second GUI.",
+        "We made it more interesting...",
+        "...by making this label interactive.",
+        "Go on, click on it again.",
+    ]
+    def __init__(self, master):
+        self.master = master
+        master.title("A simple GUI")
+
+        self.label_index = 0
+        self.label_text = StringVar()
+        self.label_text.set(self.LABEL_TEXT[self.label_index])
+        self.label = Label(master, textvariable=self.label_text)
+        self.label.bind("<Button-1>", self.cycle_label_text)
+        self.label.pack()
+
+        self.greet_button = Button(master, text="Greet", command=self.greet)
+        self.greet_button.pack()
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.pack()
+
+    def greet(self):
+        print("Greetings!")
+
+    def cycle_label_text(self, event):
+        self.label_index += 1
+        self.label_index %= len(self.LABEL_TEXT) # wrap around
+        self.label_text.set(self.LABEL_TEXT[self.label_index])
 
 if __name__ == '__main__':
     root = Tk()
